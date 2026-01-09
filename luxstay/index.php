9d6841a08bec4b1c9ec52f515b2273303d9bfb7b
xhessi <xhesildacoro1@gmail.com>
2026-01-09 22:14:07 +0100

    Create About and Facilities pages

--- luxstay/index.php
+++ luxstay/index.php
@@ -6,11 +6,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LuxStay Hotel</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Merienda&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css"/>
+    <title>LuxStay Hotel - HOME</title>
+    
+    <?php require('inc/links.php'); ?>
     <style>
         *{
             font-family: "Poppins", sans-serif;
@@ -55,128 +53,7 @@
 </head>
 <body>
 
-<!--Navigation Bar-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
-    <div class="container-fluid">
-        <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">LuxStay</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link me-2" href="#">Rooms</a>
-            </li>
-             <li class="nav-item">
-            <a class="nav-link me-2" href="#">Facilities</a>
-            </li>
-             <li class="nav-item">
-            <a class="nav-link me-2" href="#">Contact us</a>
-            </li>
-             <li class="nav-item">
-            <a class="nav-link me-2" href="#">About</a>
-            </li>
-        </ul>
-        <div class="d-flex">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-            <button type="button" class="btn btn-outlin-dark shadow-none me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#loginModal">
-            Login
-            </button>
-            <button type="button" class="btn btn-outlin-dark shadow-none me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#registerModal">
-            Register
-            </button>
-        </div>
-        </div>
-    </div>
-    </nav>
-
-<!--Login Button-->
-    <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <form action="">
-                    <div class="modal-header">
-                        <h5 class="modal-title d-flex align-items-center">
-                            <i class="bi bi-person-circle fs-3 me-2"></i>
-                            User Login</h5>
-                        <button type="reser" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="mb-3">
-                            <label class="form-label">Email address</label>
-                            <input type="email" class="form-control shadow-none">
-                           
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Password</label>
-                            <input type="password" class="form-control shadow-none">
-                           
-                        </div>
-                        <div class="d-flex align-items-center justify-content-between mb-2">
-                            <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
-                            <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
-                        </div>
-
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Login</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
-<!--Register Button-->
-    <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <form action="">
-                    <div class="modal-header">
-                        <h5 class="modal-title d-flex align-items-center">
-                            <i class="bi bi-person-lines-fill fs-3 me-2"></i>
-                            User Registration</h5>
-                        <button type="reser" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">Note: Your details must match your Id that will be required during check-in.</span>
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-md-6 ps-0 mb-3">
-                                    <label class="form-label">Name</label>
-                                    <input type="text" class="form-control shadow-none">
-                                    <br>
-                                    <label class="form-label">Email</label>
-                                    <input type="email" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 ps-0 mb-3">
-                                    <label class="form-label">Phone Number</label>
-                                    <input type="number" class="form-control shadow-none">
-                                    <br>
-                                    <label class="form-label">Date of Birth</label>
-                                    <input type="date" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 ps-0 mb-3">
-                                    <label class="form-label">Password</label>
-                                    <input type="password" class="form-control shadow-none">
-                                    <br>
-                                    <label class="form-label">Confirm Password</label>
-                                    <input type="password" class="form-control shadow-none">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
-                        </div>
-
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+<?php require('inc/header.php'); ?>
 
 <!--Picture Crousel-->
     <div class="container-fluid">
@@ -479,35 +356,8 @@
         </div>
     </div>
 
-<!--Footer-->  
-    <div class="container-fluid bg-white mt-5">
-        <div class="row">
-            <div class="col-lg-4 p-4">
-                <h3 class="h-font fw-bold fs-3 mb-2">LuxStay Hotel</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. 
-                    Quisque faucibus ex sapien vitae pellentesque sem placerat. 
-                    In id cursus mi pretium tellus duis convallis. Tempus 
-                    leo eu aenean sed diam urna tempor. Pulvinar vivamus 
-                    fringilla lacus nec metus bibendum egestas. </p>
-            </div>
-            <div class="col-lg-4 p-4">
-                <h5 class="mb-3">Links</h5>
-                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
-                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
-                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
-                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a><br>
-                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
-            </div>
-            <div class="col-lg-4 p-4">
-                <h5 class="mb-3">Follow Us!</h5>
-                <a href="#" class="d-inline-block text-decoration-none text-dark mb-2"><i class="bi bi-twitter me-1"></i>Twitter</a><br>
-                <a href="#" class="d-inline-block text-decoration-none text-dark mb-2"><i class="bi bi-instagram me-1"></i>Instagram</a><br>
-                <a href="#" class="d-inline-block text-decoration-none text-dark mb-2"><i class="bi bi-facebook me-1"></i></i>Facebook</a><br>    
-            </div>
-        </div>
-    </div>
+    <?php require('inc/footer.php'); ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
     <script>
         var swiper = new Swiper(".swiper-container", {
