3e59a1e719293bd7a35c7660894c6ffed23f466a
Xhoana Pepa <xhoana.pepa7@gmail.com>
2026-01-13 01:05:26 +0100

    xhoit i funx e tera shum mir

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -10,18 +10,41 @@
     die("Can not connect to Database".mysqli_connect_error());
  }
  
-     function filteration($data){
-        foreach($data as $key => $value){
+   function filteration($data){
+    foreach($data as $key => $value){
+        if (is_array($value)) {
+            $data[$key] = $value;
+        } else {
             $value = trim($value);
             $value = stripslashes($value);
-            $value = htmlspecialchars($value);
             $value = strip_tags($value);
+            $value = htmlspecialchars($value, ENT_QUOTES);
             $data[$key] = $value;
         }
-        return $data;
     }
+    return $data;
+}
 
-
+function update($sql,$values,$datatypes)
+{
+    $con = $GLOBALS['con'];
+    if($stmt = mysqli_prepare($con,$sql))
+    {
+        mysqli_stmt_bind_param($stmt,$datatypes,...$values);
+        if(mysqli_stmt_execute($stmt)){
+            $res = mysqli_stmt_affected_rows($stmt);
+            mysqli_stmt_close($stmt);
+            return $res;
+        }
+        else{
+            mysqli_stmt_close($stmt);
+            die("Query can not be executed - Update");
+        }
+    }
+    else{
+        die("Query can not be prepared - Update");
+    }
+}
 
   function select($sql,$values,$datatypes)
   {
@@ -37,15 +60,53 @@
             die("Query can not be executed - Select");
 
         }
-       
-
     }
     else{
         die("Query can not be prepared - Select");
     }
   }
 
+  function insert($sql,$values,$datatypes)
+{
+    $con = $GLOBALS['con'];
+    if($stmt = mysqli_prepare($con,$sql))
+    {
+        mysqli_stmt_bind_param($stmt,$datatypes,...$values);
+        if(mysqli_stmt_execute($stmt)){
+            $res = mysqli_stmt_affected_rows($stmt);
+            mysqli_stmt_close($stmt);
+            return $res;
+        }
+        else{
+            mysqli_stmt_close($stmt);
+            die("Query can not be executed - Insert");
+        }
+    }
+    else{
+        die("Query can not be prepared - Insert");
+    }
+}
 
+function delete($sql,$values,$datatypes)
+{
+    $con = $GLOBALS['con'];
+    if($stmt = mysqli_prepare($con,$sql))
+    {
+        mysqli_stmt_bind_param($stmt,$datatypes,...$values);
+        if(mysqli_stmt_execute($stmt)){
+            $res = mysqli_stmt_affected_rows($stmt);
+            mysqli_stmt_close($stmt);
+            return $res;
+        }
+        else{
+            mysqli_stmt_close($stmt);
+            die("Query can not be executed - Delete");
+        }
+    }
+    else{
+        die("Query can not be prepared - Delete");
+    }
+}
 
 
 ?>
--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -7,57 +7,34 @@ define("ABOUT_IMG_PATH", SITE_URL . "images/about/");
 define("UPLOAD_IMAGE_PATH", $_SERVER['DOCUMENT_ROOT'] . "/LuxStay/images/"); // rregulluar
 define("ABOUT_FOLDER", "about/");
 
-
 function adminlogin()
 {
-    session_start();
+
     if (!(isset($_SESSION['adminlogin']) && $_SESSION['adminlogin'] === true)) {
         echo "<script>window.location.href='index.php';</script>";
         exit;
     }
-    session_regenerate_id(true);
 }
-
-
-function redirect($url)
-{
-    echo "<script>window.location.href='$url';</script>";
+function redirect($url){
+    echo "<script>
+    window.location.href='$url';
+    </script>";
     exit;
 }
+function alert($type,$msg){
+    $bs_class = ($type == 'success') ? "alert-success" : "alert-danger";
 
-
-function alert($type, $msg, $timeout = 1000)
-{
-    // Zgjidh klasin bootstrap sipas tipit
-    $bs_class = ($type === "success") ? "alert-success" : "alert-danger";
-
-    // Shfaq alert-in
     echo <<<alert
-<div class="alert $bs_class fade show custom-alert" role="alert">
+    <div class="alert $bs_class alert-dismissible fade show custom-alert" role="alert">
     <strong class="me-3">$msg</strong>
-</div>
-
-<script>
-// Pasi faqja të ngarkohet
-setTimeout(() => {
-    const el = document.querySelector('.custom-alert');
-    if(el) {
-        // heq 'show' për animacion fade-out
-        el.classList.remove('show');
-        // largon alert nga DOM pas 500ms për t'i dhënë kohë animacionit
-        setTimeout(() => el.remove(), 500);
-    }
-}, $timeout);
-</script>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
 alert;
+
 }
-
-
-
-
-function uploadImage($image, $folder)
+function uploadImage($image,$folder)
 {
-    $valid_mime = ['image/jpeg', 'image/png', 'image/webp'];
+    $valid_mime = ['image/jpeg','image/png','image/webp'];
     $img_mime = $image['type'];
 
     if (!in_array($img_mime, $valid_mime)) {
@@ -66,7 +43,7 @@ function uploadImage($image, $folder)
         return 'inv_size';
     } else {
         $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
-        $rname = 'IMG_' . random_int(11111, 99999) . ".$ext";
+        $rname = 'IMG_' . random_int(11111, 99999) . "." . $ext;
         $img_path = UPLOAD_IMAGE_PATH . $folder . $rname;
 
         if (move_uploaded_file($image['tmp_name'], $img_path)) {
@@ -77,16 +54,19 @@ function uploadImage($image, $folder)
     }
 }
 
- 
+
 function deleteImage($image, $folder)
 {
-    $path = UPLOAD_IMAGE_PATH . $folder . $image;
-    if (file_exists($path)) {
-        return unlink($path);
+    // provo ta fshish foton nga serveri
+    if(unlink(UPLOAD_IMAGE_PATH.$folder.$image)){
+        // nqs u fshi me sukses kthe true
+        return true;
     }
-    return false;
+    else{
+        // nqs deshton, kthe false
+        return false;
+    }
 }
-
 
 function selectAll($table)
 {
@@ -94,45 +74,5 @@ function selectAll($table)
     $res = mysqli_query($con, "SELECT * FROM $table");
     return $res;
 }
-
-function insert($query, $values, $types) {
-    global $con; 
-
-    $stmt = mysqli_prepare($con, $query);
-    if(!$stmt){
-        die("Prepared statement failed: " . mysqli_error($con));
-    }
-
-    if(!empty($values)){
-        mysqli_stmt_bind_param($stmt, $types, ...$values);
-    }
-
-    $res = mysqli_stmt_execute($stmt);
-
-    mysqli_stmt_close($stmt);
-
-    return $res; 
-}
-
-function update($query, $values = [], $types = "") {
-    global $con; // përdor lidhjen globale me DB
-
-    $stmt = mysqli_prepare($con, $query);
-    if(!$stmt){
-        die("Prepared statement failed: " . mysqli_error($con));
-    }
-
-    if(!empty($values)){
-        mysqli_stmt_bind_param($stmt, $types, ...$values);
-    }
-
-    $res = mysqli_stmt_execute($stmt);
-
-    mysqli_stmt_close($stmt);
-
-    return $res; // true nëse punoi, false nëse dështoi
-}
-
-
 
 ?>
